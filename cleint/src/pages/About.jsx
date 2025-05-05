6a5d56d2aff28d0ba489280619ba2f80421c2fa2
mujahid1243 <muslim25351@gmail.com>
2025-05-05 09:12:57 +0300

    complete about page

--- cleint/src/pages/About.jsx
+++ cleint/src/pages/About.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1 className="text-5xl font-bold pl-7 my-10 text-slate-600">
+        <span className="text-slate-400 text-4xl font-bold lg:text-5xl">
+          Welcome to
+        </span>{" "}
+        luxiary real<span>Estate</span>
+      </h1>
+      <p className="text-md  mt-10 text-slate-500 max-w-6xl pl-8">
+        At luxiary realEstate, we help clients find the perfect home or property
+        investment with confidence. Our team brings deep expertise in the local
+        market and a commitment to exceptional service. Whether you're buying,
+        selling, or renting, we guide you every step of the way. We believe real
+        estate is more than transactions—it’s about building lasting
+        relationships. Transparency, integrity, and results are the core of our
+        business. Let us help you turn your real estate goals into reality.
+      </p>
+      <p className="text-md my-3 text-slate-500 max-w-6xl pl-8">
+        The goal of our real estate website is to provide a user-friendly
+        platform where buyers, sellers, and renters can easily connect, explore
+        listings, and make informed property decisions. We aim to simplify the
+        real estate process through accessible tools, clear information, and
+        expert support.
+      </p>
+    </div>
+  );
 }
